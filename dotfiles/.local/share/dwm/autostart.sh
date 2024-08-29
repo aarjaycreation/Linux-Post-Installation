959ea725e2884c477bf53d855b85c1b7a10e4a68
sevu11 <johan.sorman@icloud.com>
2024-08-29 16:17:05 +0000

    Adding dotfiles folder

---
+++ dotfiles/.local/share/dwm/autostart.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+xrandr -s 1920x1080 &
+
+slstatus &
+
+# polkit
+/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
+
+# background
+feh --randomize --bg-fill ~/.config/backgrounds/* &
+#feh --randomize --bg-scale ~/.config/backgrounds/* &
+
+
+
+# compositor
+picom --animations -b &
+
+# sxhkd
+# (re)load sxhkd for keybinds
+if hash sxhkd >/dev/null 2>&1; then
+	pkill sxhkd
+	sleep 0.5
+	sxhkd -c "$HOME/.config/suckless/sxhkd/sxhkdrc" &
+fi
+
+# Notifications
+dunst &
+
+
